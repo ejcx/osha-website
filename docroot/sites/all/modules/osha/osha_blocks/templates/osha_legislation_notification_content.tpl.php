06243d549ca483c5d917eef27e2de79b57f565d7
Andrei Melis <andrei.melis@eaudeweb.ro>
2015-07-30 17:25:30 +0300

    CW-1812 Notification in the legislation pages

---
+++ docroot/sites/all/modules/osha/osha_blocks/templates/osha_legislation_notification_content.tpl.php
@@ -0,0 +1,3 @@
+<div class="osha-legislation-notification">
+  <?php print t('We are currently revising the information in this section, so for the moment, you might find some broken links or some out of date information that will be corrected in the coming weeks, so if this happens, we invite you to visit us again soon.');?>
+</div>
