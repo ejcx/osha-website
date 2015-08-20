185958086e910f78b1a6f4be5eac45fe6157ae31
e <e@es-MacBook-Pro.attlocal.net>
2015-08-19 19:21:39 -0700

    Fix file inclusion vulnerability

--- docroot/site_call.php
+++ docroot/site_call.php
@@ -1,6 +1,10 @@
 <?php
 
 $site = $_GET['site'];
+$urlparts = parse_url($site);
+if ($urlparts["scheme"] == "file") {
+	exit;
+}
 $s = curl_init();
 curl_setopt($s, CURLOPT_URL, $site);
 curl_setopt($s, CURLOPT_RETURNTRANSFER, TRUE);
